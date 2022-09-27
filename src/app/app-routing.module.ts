119572fe43bd4df6cfac45c6aa96de9dda68b9a6
Matheus Christo <matheuschristo.09@gmail.com>
2022-09-27 16:19:56 -0300

    Atualizado app-routing.module e app.component.html

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,25 @@
-import { Routes, RouterModule } from '@angular/router';
+import { HomeEstablishmentComponent } from './components/establishment/home-establishment/home-establishment.component';
+import { EstablishmentConsumableComponent } from './components/establishment/establishment-consumable/establishment-consumable.component';
+import { ClientConsumableComponent } from './components/_client/client-consumable/client-consumable.component';
+import { HomeClientComponent } from './components/_client/home-client/home-client.component';
+import { CadastroEstablishmentComponent } from './components/establishment/cadastro/cadastro.component';
+import { CadastroClienteComponent } from './components/_client/cadastro/cadastro.component';
+import { LoginComponent } from './components/user/login/login.component';
+
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '',component: LoginComponent },
+  {path: 'cadastroC',component: CadastroClienteComponent},
+  {path: 'cadastroE',component: CadastroEstablishmentComponent},
+  {path: 'home-client/:id', component: HomeClientComponent},
+  {path: 'consumablesC/:idEstablishment', component: ClientConsumableComponent},
+  {path: 'consumablesE/:idEstablishment', component: EstablishmentConsumableComponent},
+  {path: 'home-establishment/:id',component: HomeEstablishmentComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
