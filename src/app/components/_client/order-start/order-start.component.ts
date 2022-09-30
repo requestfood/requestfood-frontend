6ae52eeb0d111eb690551295130c09e10c2abeb6
Matheus Christo <matheuschristo.09@gmail.com>
2022-09-29 21:37:22 -0300

    Criado component "order-start"

---
+++ src/app/components/_client/order-start/order-start.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-order-start',
+  templateUrl: './order-start.component.html',
+  styleUrls: ['./order-start.component.css']
+})
+export class OrderStartComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
