709b5a63ec16cc8dfb61510bfcb6e95be5e45e0a
Fellipe Naudo Ferreira <felipenaudof@gmail.com>
2022-09-30 16:12:34 -0300

    Criado EstablishmentProfileComponent

---
+++ src/app/components/user/user-update/establishment/profile-update/profile-update.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-profile-update',
+  templateUrl: './profile-update.component.html',
+  styleUrls: ['./profile-update.component.css']
+})
+export class EstablishmentProfileUpdateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
