87ddfcc0e9e3c63da17e62f5387fbc82dfe53082
Fellipe Naudo Ferreira <felipenaudof@gmail.com>
2022-09-30 16:13:14 -0300

    ClientProfileUpdateComponent

---
+++ src/app/components/user/user-update/_client/profile-update/profile-update.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-profile-update',
+  templateUrl: './profile-update.component.html',
+  styleUrls: ['./profile-update.component.css']
+})
+export class ClientProfileUpdateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
