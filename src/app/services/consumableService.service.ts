69ecf43288b9da04c896b6de6699d112c90d80b7
Matheus Christo <matheuschristo.09@gmail.com>
2022-10-02 05:55:47 -0300

    Criado consumableService

---
+++ src/app/services/consumableService.service.ts
@@ -0,0 +1,27 @@
+import { ConsumableCard } from './../models/EstablishmentWithConsumables';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ConsumableService {
+  
+  currentConsumable: ConsumableCard = {
+    id: 0,
+    image: null,
+    name: "",
+    price: 0,
+    description: ""
+  }
+
+  getCurrentConsumable() : ConsumableCard{
+    return this.currentConsumable
+  }
+
+  setCurrentConsumable(currentConsumable: ConsumableCard) {
+    this.currentConsumable = currentConsumable
+  }
+
+  constructor() { }
+
+}
