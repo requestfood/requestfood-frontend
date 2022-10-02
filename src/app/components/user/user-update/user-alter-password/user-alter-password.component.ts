70657d017292be05d9dffc0b0aaa990de5cd2c61
Fellipe Naudo Ferreira <felipenaudof@gmail.com>
2022-10-02 02:42:35 -0300

    Criado UserAlterPasswordComponent

---
+++ src/app/components/user/user-update/user-alter-password/user-alter-password.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-alter-password',
+  templateUrl: './user-alter-password.component.html',
+  styleUrls: ['./user-alter-password.component.css']
+})
+export class UserAlterPasswordComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
