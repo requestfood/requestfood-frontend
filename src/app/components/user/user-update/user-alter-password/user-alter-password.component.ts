edd530f3e781d500b9956512697134df15e2d9c3
Fellipe Naudo Ferreira <felipenaudof@gmail.com>
2022-10-02 22:01:38 -0300

    Atualizado CSS de UserAlterPasswordComponent

--- src/app/components/user/user-update/user-alter-password/user-alter-password.component.ts
+++ src/app/components/user/user-update/user-alter-password/user-alter-password.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-user-alter-password',
   templateUrl: './user-alter-password.component.html',
-  styleUrls: ['./user-alter-password.component.css']
+  styleUrls: ['../_client/contact-update/contact-update.component.css']
 })
 export class UserAlterPasswordComponent implements OnInit {
 
