8900614d8f7e629789aaaa41ed14f57cbb2ca5bf
Fellipe Naudo Ferreira <felipenaudof@gmail.com>
2022-10-07 11:18:16 -0300

    Criado ItemInfoComponent

---
+++ src/app/components/_client/order-start/bag-items/item-info/item-info.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-item-info',
+  templateUrl: './item-info.component.html',
+  styleUrls: ['./item-info.component.css']
+})
+export class ItemInfoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
