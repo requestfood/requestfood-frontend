344f7a2cc5f492528f5fc1c09c80f11f08e41480
Fellipe Naudo Ferreira <felipenaudof@gmail.com>
2022-10-07 00:03:57 -0300

    Criado BagItemsComponent

---
+++ src/app/components/_client/order-start/bag-items/bag-items.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bag-items',
+  templateUrl: './bag-items.component.html',
+  styleUrls: ['./bag-items.component.css']
+})
+export class BagItemsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
