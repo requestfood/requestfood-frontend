6061b10a5926c806cded2d20c529441e4623284c
Fellipe Naudo Ferreira <felipenaudof@gmail.com>
2022-10-10 17:19:54 -0300

    Criado Models Dish e Drink

---
+++ src/app/models/consumables/drink.ts
@@ -0,0 +1,11 @@
+export interface Drink{
+
+    id: Number
+    idEstablishment: Number
+    name: String
+    price: Number
+    description: String,
+    categoryDrink: String,
+    alcoholic: Boolean,
+    image: any
+}
