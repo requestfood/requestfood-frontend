e64d879c68a5e35e0eb9589af5765dbe3a01009e
Kaua Berman Amorim <77868006+kauabamorim@users.noreply.github.com>
2022-10-13 17:28:39 -0300

    Criada Comandas Estabelecimento

---
+++ src/app/components/establishment/establishment-order/establishment-order.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-establishment-order',
+  templateUrl: './establishment-order.component.html',
+  styleUrls: ['./establishment-order.component.css']
+})
+export class EstablishmentOrderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
