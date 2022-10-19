1713a0d006e2198942e3c3cf13428ed6e39c6e8e
Matheus Christo da Silva <matheuschristo.09@gmail.com>
2022-10-19 16:33:26 -0300

    Atualizado ImageComponent

--- src/app/components/core/image-register/image.component.ts
+++ src/app/components/core/image-register/image.component.ts
@@ -1,6 +1,5 @@
 import { ImageService } from 'src/app/services/core/image.service';
-import { UserService } from './../../../services/User.service';
-import { ActivatedRoute, Router } from '@angular/router';
+import { Router } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import { EstablishmentService } from './../../../services/EstablishmentService.service';
 import { Component, OnInit, Input } from '@angular/core';
@@ -23,10 +22,14 @@ export class ImageComponent implements OnInit {
 
   open: boolean = true
 
-  constructor(private service: EstablishmentService,
+  uploadImage: File = new File(["foo"], "foo.txt", { type: "text/plain", })
+
+  postResponse: any;
+
+  successResponse: string = ""
+
+  constructor(
     private httpClient: HttpClient,
-    private actRouter: ActivatedRoute,
-    private userService: UserService,
     private imageService: ImageService,
     private router: Router
   ) { }
@@ -34,31 +37,14 @@ export class ImageComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  uploadImage: File = new File(["foo"], "foo.txt", { type: "text/plain", })
-  postResponse: any;
-  successResponse: string = "";
-
-
   public onFileSelected(event: any) {
     this.uploadImage = event.target.files[0];
-
-    console.log(event.target.files[0]);
-    
   }
 
-  
-
-  
   onSkip() {
     if (this.text.typeObject == 'ESTABLISHMENT') {
 
-      let image = '../../../../assets/profile/perfil-default.png'
-
-      console.log(image);
-      
-      
     } else if (this.text.typeObject == 'DRINK') {
-
 
     }else if (this.text.typeObject == 'DISH') {
     }
@@ -67,24 +53,24 @@ export class ImageComponent implements OnInit {
   }
 
   imageUpload(type: string = this.text.typeObject) {
-  
+
     if (type == 'ESTABLISHMENT') {
 
       const imageFormData = new FormData()
       imageFormData.append('image', this.uploadImage, this.uploadImage.name)
-  
+
       this.httpClient.post('http://localhost:8080/establishment/image/' + this.text.id, imageFormData, { observe: 'response' })
         .subscribe((response: any) => {
           this.postResponse = response
           this.successResponse = this.postResponse.body.message
         });
         this.router.navigate(['user-update/profile-establishment/' + this.text.id])
-        
+
     } else if (type == 'DRINK') {
 
       const imageFormData = new FormData()
       imageFormData.append('image', this.uploadImage, this.uploadImage.name)
-  
+
       this.httpClient.post('http://localhost:8080/drink/image/' + this.text.id, imageFormData, { observe: 'response' })
         .subscribe((response: any) => {
           this.postResponse = response
@@ -92,10 +78,10 @@ export class ImageComponent implements OnInit {
         });
 
     }else if (type == 'DISH') {
-  
+
       const imageFormData = new FormData()
       imageFormData.append('image', this.uploadImage, this.uploadImage.name)
-  
+
       this.httpClient.post('http://localhost:8080/dish/image/' + this.text.id, imageFormData, { observe: 'response' })
         .subscribe((response: any) => {
           this.postResponse = response
@@ -104,6 +90,5 @@ export class ImageComponent implements OnInit {
     }
 
     location.reload()
-
   }
 }
